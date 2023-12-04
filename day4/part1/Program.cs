1efc1adcc8179e88758cc084eb7ff04932db42b1
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-04 23:55:40 +0100

    simplify char array

--- day4/part1/Program.cs
+++ day4/part1/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine(cards.FullScore());
 
 Console.WriteLine(
     File.ReadAllLines("input.txt")
-        .Select(s => s.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries))
+        .Select(s => s.Split(new char[0], StringSplitOptions.RemoveEmptyEntries))
         .Select(n => n[..12].Intersect(n[12..]))
         .Select(n => (1 << n.Count()) / 2)
         .Sum());
