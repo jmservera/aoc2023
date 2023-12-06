4ba573d11a52ccd82c72a237fdebd8203ea5a02a
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-06 16:23:16 +0100

    extract calculations

--- day6/part1/Program.cs
+++ day6/part1/Program.cs
@@ -2,22 +2,24 @@
 
 var result = lines[0].Split(new char[0], StringSplitOptions.RemoveEmptyEntries)[1..].Select(x => int.Parse(x))
     .Zip(lines[1].Split(new char[0], StringSplitOptions.RemoveEmptyEntries)[1..].Select(x => int.Parse(x)),
-    (a, b) => (a, b))
-    .Select(x => ((x.a + Math.Sqrt((x.a * x.a) - 4 * x.b)) / 2, (x.a - Math.Sqrt((x.a * x.a) - 4 * x.b)) / 2))
-    .Select(x => (x.Item1 > x.Item2) ? (x.Item1, x.Item2) : (x.Item2, x.Item1))
-    .Select(x => (Math.Ceiling(x.Item1) - 1, Math.Floor(x.Item2) + 1))
-    .Select(x => x.Item1 - x.Item2 + 1)
-    .Aggregate((a, b) => a * b);
+    (a, b) => ((long)a, (long)b))
+    .Calc();
 
 Console.WriteLine(result);
 
 long a = long.Parse(string.Concat(lines[0].Split(new char[0], StringSplitOptions.RemoveEmptyEntries)[1..]));
 long b = long.Parse(string.Concat(lines[1].Split(new char[0], StringSplitOptions.RemoveEmptyEntries)[1..]));
 
-var result2 = new[] { (a, b) }.Select(x => ((x.a + Math.Sqrt((x.a * x.a) - 4 * x.b)) / 2, (x.a - Math.Sqrt((x.a * x.a) - 4 * x.b)) / 2))
+var result2 = new[] { (a, b) }.Calc();
+Console.WriteLine(result2);
+
+public static class Extensions
+{
+    public static double Calc(this IEnumerable<(long a, long b)> x) =>
+    x.Select(x => ((x.a + Math.Sqrt((x.a * x.a) - 4 * x.b)) / 2, (x.a - Math.Sqrt((x.a * x.a) - 4 * x.b)) / 2))
     .Select(x => (x.Item1 > x.Item2) ? (x.Item1, x.Item2) : (x.Item2, x.Item1))
     .Select(x => (Math.Ceiling(x.Item1) - 1, Math.Floor(x.Item2) + 1))
     .Select(x => x.Item1 - x.Item2 + 1)
     .Aggregate((a, b) => a * b);
-Console.WriteLine(result2);
+}
 
