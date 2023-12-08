086e4f12692d04e1dc3a400bd8e27187628fa15b
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-08 12:53:17 +0100

    lcm

--- day8/part2/Program.cs
+++ day8/part2/Program.cs
@@ -1,22 +1,40 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 
+static long gcf(long a, long b)
+{
+    while (b != 0)
+    {
+        long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+static long lcm(long a, long b)
+{
+    return (a / gcf(a, b)) * b;
+}
+
 var moves = lines[0];
 var map = lines[2..].Select(l => new { p = l[..3], left = l[7..10], right = l[12..15] }).ToDictionary(x => x.p, x => (x.left, x.right));
 
 var p = map.Where(m => m.Key[2] == 'A').Select(m => m.Key).ToArray();
-int steps = 0;
-do
+long[] steps = new long[p.Length];
+for (int i = 0; i < p.Length; i++)
 {
-
-    Parallel.For(0, p.Length, (i) =>
+    do
     {
         foreach (var c in moves)
         {
             p[i] = c == 'R' ? map[p[i]].right : map[p[i]].left;
         }
-    });
-    steps += moves.Length;
+        steps[i] += moves.Length;
+    }
+    while (p[i][2] != 'Z');
 }
-while (!p.All(s => s[2] == 'Z'));
 
-Console.WriteLine(steps);
+steps.ToList().ForEach(c => Console.WriteLine(c));
+
+
+Console.WriteLine(steps.Aggregate(lcm));
