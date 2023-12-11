a6bf86b29aca15a3b87017695dabad536ba0a5fc
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-11 14:41:35 +0100

    part 2

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,44 +1,46 @@
-﻿var lines = File.ReadAllLines("test.txt");
+﻿var lines = File.ReadAllLines("input.txt");
 
-var expanded = new List<List<char>>();
 var cols = new bool[lines[0].Length];
-foreach (var line in lines)
-{
-    if (!line.Contains('#'))
-    {
-        expanded.Add(line.ToCharArray().ToList());
-    }
-    expanded.Add(line.ToCharArray().ToList());
 
-    line.Select((c, i) => (c == '#', i)).ToList().ForEach(a => cols[a.i] |= a.Item1);
+var expanded = lines.Select(line =>
+                            {
+                                line.Select((c, i) => (c == '#', i)).ToList().ForEach(a => cols[a.i] |= a.Item1); // mark columns with galaxies
+                                return line.Contains('#') ? line.ToCharArray() : Enumerable.Repeat('H', line.Length); // mark rows without galaxies
+                            })
+                        .ToList()
+                        .Select(line => line.Select(
+                            (c, i) => cols[i] ? c : 'V')); // mark columns without galaxies
+
+
+static long Calc(IEnumerable<IEnumerable<char>> map, long n)
+{
+    long h = 0;
+
+    var coordinates = map.SelectMany(line =>
+        {
+            h += line.First() == 'H' ? n : 1; // increment horizontal coordinate
+            long v = 0;
+            return line.Select(c =>
+                {
+                    v += c == 'V' ? n : 1; // increment vertical coordinate
+                    return (h, v, c);
+                })
+                .Where(t => t.c == '#') // filter out galaxies
+                .Select(t => (x: t.h, y: t.v));
+        })
+        .ToList();
+
+    return coordinates.Join(coordinates, c => true, c => true, (a, b) => (a, b)) // Cartesian product
+                        .Where(c => c.a.x != c.b.x || c.a.y != c.b.y) // Remove self
+                        .Select(c => new[] { c.a, c.b }
+                                                .OrderBy(c => c.x)
+                                                .ThenBy(c => c.y)
+                                                .Chunk(2)
+                                                .Select(c => (a: c[0], b: c[1])).First()) // Switch to ordered pairs
+                        .Distinct() // Remove duplicates
+                        .Select(c => Math.Abs(c.a.x - c.b.x) + Math.Abs(c.a.y - c.b.y)) // Calculate distance
+                        .Sum();
 }
 
-cols.Select((b, i) => (b, i)).Reverse().ToList().ForEach(c =>
-{
-    if (!c.b)
-    {
-        expanded.ForEach(r => r.Insert(c.i, '.'));
-    }
-});
-
-expanded.ForEach(r => Console.WriteLine(new string(r.ToArray())));
-
-var coordinates = expanded.Select((l, x) => (l, x))
-    .Where(line => line.l.Contains('#'))
-    .Select(line => line.l.Select((c, y) => (c, y)).Where(l => l.c == '#').Select(c => (line.x, c.y)))
-    .SelectMany(c => c).Select(c => (c.x, c.y)).ToList();
-
-
-var sum = coordinates.Join(coordinates, c => true, c => true, (a, b) => (a, b))
-   .Where(c => c.a.x != c.b.x || c.a.y != c.b.y)
-   .Select(c => new[] { c.a, c.b }
-                        .OrderBy(c => c.x)
-                        .ThenBy(c => c.y)
-                        .Chunk(2)
-                        .Select(c => (a: c[0], b: c[1])).First())
-   .Distinct()
-   .Select(c => Math.Abs(c.a.x - c.b.x) + Math.Abs(c.a.y - c.b.y))
-   .Sum();
-
-Console.WriteLine(sum);
-
+Console.WriteLine(Calc(expanded, 2));
+Console.WriteLine(Calc(expanded, 1000000));
