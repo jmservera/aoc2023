f538559a9872c5ae76f41484db0066c7769bb7f4
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-12 14:21:24 +0100

    part2 without optimizing

--- day12/Program.cs
+++ day12/Program.cs
@@ -1,25 +1,30 @@
 ﻿
-var lines = File.ReadAllLines("input.txt");
+var lines = File.ReadAllLines("test.txt");
+
+int counter = 0;
 
 var result = lines.Select(line => line.Split(' '))
         .Select(line => (pattern: line[0], values: line[1].Split(',').Select(int.Parse).ToArray()))
+        .AsParallel()
         .Sum(line =>
         {
-            Console.WriteLine($"{line.pattern} {string.Join(',', line.values)}");
+            var i = Interlocked.Increment(ref counter);
+            Console.WriteLine($"${i}: {line.pattern} {string.Join(',', line.values)}");
             var r = Count(line.pattern, line.values);
-            Console.WriteLine($"{line.pattern} {string.Join(',', line.values)} {r}");
+            Console.WriteLine($"${i}: {line.pattern} {string.Join(',', line.values)} {r}");
             return r;
         });
 
 
-long Count(string pattern, int[] values, bool first = true, string answer = "")
+long Count(string pattern, int[] values, bool first = true)
 {
+    if (pattern.Length < values.Sum() + values.Length - 1)
+        return 0;
     long result = 0L;
     if (pattern.Length == 0)
     {
         if (values.Length == 0 || (values.Length == 1 && values[0] == 0))
         {
-            Console.WriteLine(answer);
             return 1;
         }
         else
@@ -32,7 +37,6 @@ long Count(string pattern, int[] values, bool first = true, string answer = "")
             return 0;
         else
         {
-            Console.WriteLine(answer + ".");
             return 1;
         }
     }
@@ -42,14 +46,14 @@ long Count(string pattern, int[] values, bool first = true, string answer = "")
         if (pattern[0] == '#')
             return 0;
         else
-            result += Count(pattern[1..], values[1..], true, answer + ".");
+            result += Count(pattern[1..], values[1..], true);
     }
     else
     {
         if (pattern[0] == '.')
         {
             if (first)
-                result += Count(pattern[1..], values, first, answer + ".");
+                result += Count(pattern[1..], values, first);
             else
                 return 0;
         }
@@ -57,14 +61,33 @@ long Count(string pattern, int[] values, bool first = true, string answer = "")
         {
             if (pattern[0] == '?' && first)
             {
-                result += Count(pattern[1..], values, true, answer + ".");
+                result += Count(pattern[1..], values, true);
             }
-            values = values.Clone() as int[];
-            values[0]--;
-            result += Count(pattern[1..], values, false, answer + "#");
+            var newValues = new int[values.Length];
+            values.CopyTo(newValues, 0);
+            newValues[0]--;
+            result += Count(pattern[1..], newValues, false);
         }
     }
 
     return result;
 }
 Console.WriteLine(result);
+
+counter = 0;
+
+long unfold = lines.Select(line => line.Split(' '))
+        .Select(line => (pattern: line[0], values: line[1].Split(',').Select(int.Parse).ToArray()))
+        .Select(line => (pattern: String.Join('?', Enumerable.Repeat<string>(line.pattern, 5).ToArray()), values: Enumerable.Repeat(line.values, 5).SelectMany(x => x).ToArray(), result: Count(line.pattern, line.values)))
+        .AsParallel()
+        .Sum(line =>
+        {
+            var i = Interlocked.Increment(ref counter);
+
+            Console.WriteLine($"${i}: {line.pattern} {string.Join(',', line.values)}");
+            var r = Count(line.pattern, line.values);
+            Console.WriteLine($"${i}: {line.pattern} {string.Join(',', line.values)} {r}");
+            return r;
+        });
+
+Console.WriteLine(unfold);
