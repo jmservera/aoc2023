c303ed277ffdaab04c1fb05ba4dbe70edd1549ee
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-12 16:08:02 +0100

    Refactor Count method to use memoization

--- day12/Program.cs
+++ day12/Program.cs
@@ -5,7 +5,6 @@ int counter = 0;
 
 var result = lines.Select(line => line.Split(' '))
         .Select(line => (pattern: line[0], values: line[1].Split(',').Select(int.Parse).ToArray()))
-        .AsParallel()
         .Sum(line =>
         {
             var i = Interlocked.Increment(ref counter);
@@ -16,10 +15,21 @@ var result = lines.Select(line => line.Split(' '))
         });
 
 
-long Count(string pattern, int[] values, bool first = true)
+static long Count(string pattern, int[] values, bool first = true, Dictionary<string, long>? memo = null)
 {
     if (pattern.Length < values.Sum() + values.Length - 1)
+    {
         return 0;
+    }
+
+    memo ??= new Dictionary<string, long>();
+
+    string key = $"{pattern}|{string.Join(",", values)}|{first}";
+    if (memo.ContainsKey(key))
+    {
+        return memo[key];
+    }
+
     long result = 0L;
     if (pattern.Length == 0)
     {
@@ -28,13 +38,17 @@ long Count(string pattern, int[] values, bool first = true)
             return 1;
         }
         else
+        {
             return 0;
+        }
     }
 
     if (values.Length == 0 || (values.Length == 1 && values[0] == 0))
     {
         if (pattern.Contains('#'))
+        {
             return 0;
+        }
         else
         {
             return 1;
@@ -44,32 +58,41 @@ long Count(string pattern, int[] values, bool first = true)
     if (values[0] == 0)
     {
         if (pattern[0] == '#')
+        {
             return 0;
+        }
         else
-            result += Count(pattern[1..], values[1..], true);
+        {
+            result += Count(pattern[1..], values[1..], true, memo);
+        }
     }
     else
     {
         if (pattern[0] == '.')
         {
             if (first)
-                result += Count(pattern[1..], values, first);
+            {
+                result += Count(pattern[1..], values, first, memo);
+            }
             else
+            {
                 return 0;
+            }
         }
         else
         {
             if (pattern[0] == '?' && first)
             {
-                result += Count(pattern[1..], values, true);
+                result += Count(pattern[1..], values, true, memo);
             }
             var newValues = new int[values.Length];
             values.CopyTo(newValues, 0);
             newValues[0]--;
-            result += Count(pattern[1..], newValues, false);
+            result += Count(pattern[1..], newValues, false, memo);
         }
     }
 
+    memo[key] = result;
     return result;
 }
 Console.WriteLine(result);
@@ -79,8 +102,6 @@ counter = 0;
 long unfold = lines.Select(line => line.Split(' '))
         .Select(line => (pattern: line[0], values: line[1].Split(',').Select(int.Parse).ToArray()))
         .Select(line => (pattern: String.Join('?', Enumerable.Repeat<string>(line.pattern, 5).ToArray()), values: Enumerable.Repeat(line.values, 5).SelectMany(x => x).ToArray(), result: Count(line.pattern, line.values)))
-        .AsParallel()
-        .WithDegreeOfParallelism(160)
         .Sum(line =>
         {
             var i = Interlocked.Increment(ref counter);
