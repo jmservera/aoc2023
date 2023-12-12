de582cae250fc755273248a2e7fd92bb26824c9f
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-12 17:14:41 +0100

    Fix array declaration in Program.cs

--- day12/Program.cs
+++ day12/Program.cs
@@ -62,7 +62,7 @@ static long Count(string pattern, int[] values, bool first = true, Dictionary<st
             {
                 result += Count(pattern[1..], values, true, memo);
             }
-            var newValues = new int[values.Length];
+            int[] newValues = new int[values.Length];
             values.CopyTo(newValues, 0);
             newValues[0]--;
             result += Count(pattern[1..], newValues, false, memo);
@@ -77,7 +77,8 @@ var watch = new System.Diagnostics.Stopwatch();
 watch.Start();
 long unfold = lines.Select(line => line.Split(' '))
         .Select(line => (pattern: line[0], values: line[1].Split(',').Select(int.Parse).ToArray()))
-        .Select(line => (pattern: string.Join('?', Enumerable.Repeat(line.pattern, 5).ToArray()), values: Enumerable.Repeat(line.values, 5).SelectMany(x => x).ToArray(), result: Count(line.pattern, line.values)))
+        .Select(line => (pattern: string.Join('?', Enumerable.Repeat(line.pattern, 5).ToArray()),
+                            values: Enumerable.Repeat(line.values, 5).SelectMany(x => x).ToArray()))
         .Sum(line => Count(line.pattern, line.values));
 watch.Stop();
 
