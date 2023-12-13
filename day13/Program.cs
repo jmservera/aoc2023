cca0bd136b92af71c03f0e71be293044fed87365
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-14 00:49:42 +0100

    cleanup

--- day13/Program.cs
+++ day13/Program.cs
@@ -1,37 +1,26 @@
-
-
-using System.Security.Cryptography.X509Certificates;
-
-var lines = File.ReadAllLines("input.txt");
 int sep = 0;
-int counter = 0;
+var mirrors = File.ReadAllLines("input.txt")
+                .GroupBy(l => l.Length == 0 ? sep++ : sep, (k, g) => g.ToArray())
+                .Select(g => g.Where(l => l.Length > 0).ToArray());
 
 
 
-bool areColsEqual(string[] g, int col1, int col2)
+static int FindMirrorAxis(string[] g, int exactDiff = 0)
 {
-    for (int i = 0; i < g.Length; i++)
-    {
-        if (g[i][col1] != g[i][col2])
-        {
-            return false;
-        }
-    }
-    return true;
-}
-int Mirror(string[] g)
-{
-    int mirrorh = 0, mirrorv = 0;
+    int mirrorIndex = 0;
     for (int i = 1; i < g.Length; i++)
     {
-        if (g[i] == g[i - 1])
+        var m = exactDiff;
+        m -= g[i].CountDifferences(g[i - 1]);
+        if (m >= 0)
         {
-            mirrorh = i;
+            mirrorIndex = i;
             bool found = true;
             int x = 1;
             while (i + x < g.Length && i - x > 0)
             {
-                if (g[i + x] != g[i - x - 1])
+                m -= g[i + x].CountDifferences(g[i - x - 1]);
+                if (m < 0)
                 {
                     found = false;
                     break;
@@ -40,60 +29,49 @@ int Mirror(string[] g)
             }
             if (!found)
             {
-                mirrorh = 0;
+                mirrorIndex = 0;
             }
             else
             {
-                break;
-
+                if (m == 0)
+                    break;
+                else
+                    mirrorIndex = 0;
             }
         }
     }
+
+    return mirrorIndex;
+}
+
+int CalcMirror(string[] g, int maxDiff = 0)
+{
+    int mirrorh = FindMirrorAxis(g, maxDiff);
+    int mirrorv = 0;
     if (mirrorh == 0)
     {
-        for (int i = 1; i < g[0].Length; i++)
-        {
-            if (areColsEqual(g, i, i - 1))
-            {
-                mirrorv = i;
-                bool found = true;
-                int x = 1;
-                while (i + x < g[0].Length && i - x > 0)
-                {
-                    if (!areColsEqual(g, i + x, i - x - 1))
-                    {
-                        found = false;
-                        break;
-                    }
-                    x++;
-                }
-                if (!found)
-                {
-                    mirrorv = 0;
-                }
-                else
-                {
-                    break;
-                }
-
-            }
-        }
+        string[] transposed = g.Transpose().Select(s => new string(s.ToArray())).ToArray();
+        mirrorv = FindMirrorAxis(transposed, maxDiff);
     }
-    // if (mirrorh == 0 && mirrorv == 0)
-    // {
-    //     Console.ForegroundColor = ConsoleColor.Red;
-    //     for (int i = 0; i < g.Length; i++)
-    //     {
-    //         Console.WriteLine(g[i]);
-    //     }
-    // }
-    // Console.WriteLine($"{counter++} mirrorh: {mirrorh}, mirrorv: {mirrorv}");
-    // Console.ForegroundColor = ConsoleColor.White;
     return mirrorh * 100 + mirrorv;
 }
 
-long l = lines.GroupBy(l => l.Length == 0 ? sep++ : sep, (k, g) => g.ToArray())
-     .Select(g => g.Where(l => l.Length > 0).ToArray())
-     .Sum(Mirror);
 
+int l = mirrors.Sum(s => CalcMirror(s));
 Console.WriteLine(l);
+int l2 = mirrors.Sum(s => CalcMirror(s, 1));
+Console.WriteLine(l2);
+
+public static class IEnumerableExtensions
+{
+    public static int CountDifferences<T>(this IEnumerable<T> a, IEnumerable<T> b)
+    {
+        return a.Zip(b).Count(c => (c.First != null && !c.First.Equals(c.Second)) || (c.First == null && c.Second != null));
+    }
+
+    public static IEnumerable<IEnumerable<S>> Transpose<S>(this IEnumerable<IEnumerable<S>> g)
+    {
+        return Enumerable.Range(0, g.First().Count())
+            .Select(i => g.Select(s => s.ToArray()[i]));
+    }
+}
