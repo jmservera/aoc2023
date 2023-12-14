c0e01ef4ac2e9de80f650d083c4457487787d7b0
jmservera <8036360+jmservera@users.noreply.github.com>
2023-12-14 23:03:34 +0100

    simplify 1

--- day14/Program.cs
+++ day14/Program.cs
@@ -1,9 +1,9 @@
 var lines = File.ReadAllLines("test.txt");
 
-bool show = false;
 char[][] tilted = lines.Select(s => s.ToCharArray()).ToArray();
 
-Dictionary<string, (int, int, char[][])> cache = new Dictionary<string, (int, int, char[][])>();
+// Dictionary<string, char[][]> cache = new Dictionary<string, char[][]>();
+Dictionary<string, (string, char[][])> cache_next = new Dictionary<string, (string, char[][])>();
 
 //rotate tilted counter clockwise
 char[][] rotate(char[][] rotate)
@@ -20,34 +20,45 @@ char[][] rotate(char[][] rotate)
     return rotated;
 }
 
+static string tiltedToString(char[][] tilted)
+{
+    return tilted.Select(a => new string(a)).Aggregate((a, b) => a + b);
+}
+
 int cacheHits = 0;
 int cacheMisses = 0;
+int cacheNextHits = 0;
+int cacheNextMisses = 0;
 
-for (int cycle = 0; cycle < 10000; cycle++)
+long cycle_length = 3000;//1000000000;
+
+string nextKeyCached = "";
+string lastKey = "";
+
+for (long cycle = 0; cycle < cycle_length; cycle++)
 {
-    if (cycle % 1000000 == 0)
+    if (cycle_length > 100 && cycle % (cycle_length / 100) == 0)
     {
-        Console.WriteLine($"{((cycle) * 100.0) / 1000000000}");
+        Console.WriteLine($"{cycle * 100.0 / cycle_length}%");
+        Console.WriteLine($"Cache hits: {cacheHits}, cache misses: {cacheMisses},  next cache hits: {cacheNextHits}, next cache misses: {cacheNextMisses} next cache size: {cache_next.Count}");
     }
-    string newTilted = "", oldTilted = "";
-    for (int r = 0; r < 4; r++)
-    {
-        var key = tilted.Select(a => new string(a)).Aggregate((a, b) => a + b);
-        if (r == 0)
-            newTilted = key;
-        if (cache.ContainsKey(key))
-        {
-            Console.WriteLine($"{cache[key].Item1}, hits:{cache[key].Item2}");
-            cacheHits++;
-            tilted = cache[key].Item3;
-            cache[key] = (cache[key].Item1, cache[key].Item2 + 1, tilted);
-            continue;
-        }
-        else
-        {
-            cacheMisses++;
-        }
+    var currentTiltKey = nextKeyCached == "" ? tiltedToString(tilted) : nextKeyCached;
 
+    if (cache_next.ContainsKey(currentTiltKey))
+    {
+        cacheNextHits++;
+        nextKeyCached = cache_next[currentTiltKey].Item1;
+        tilted = cache_next[currentTiltKey].Item2;
+        lastKey = currentTiltKey;
+        continue;
+    }
+    else
+    {
+        cacheNextMisses++;
+    }
+    nextKeyCached = "";
+    for (int tr = 0; tr < 4; tr++)
+    {
         for (int i = 0; i < lines.Length - 1; i++)
         {
             for (int n = 0; n < lines.Length - 1 - i; n++)
@@ -63,18 +74,32 @@ for (int cycle = 0; cycle < 10000; cycle++)
             }
         }
         tilted = rotate(tilted);
-        cache[key] = (cache.Count, 0, tilted);
     }
-    oldTilted = newTilted;
-    newTilted = tilted.Select(a => new string(a)).Aggregate((a, b) => a + b);
-    if (oldTilted == newTilted)
+
+    if (lastKey != "")
     {
-        Console.WriteLine($"Cycle: {cycle}");
-        Console.WriteLine(newTilted);
-        break;
+        if (cache_next.ContainsKey(lastKey))
+        {
+            if (cache_next[lastKey].Item1 != currentTiltKey)
+            {
+                Console.WriteLine("last key not equal");
+            }
+            else
+            {
+                Console.WriteLine("last key equal");
+            }
+        }
+        cache_next[lastKey] = (tiltedToString(tilted), tilted);
+        Console.WriteLine($"last key: {lastKey}\nnext: {cache_next[lastKey].Item1}");
+        tilted.ToList().ForEach(a =>
+            Console.WriteLine(new string(a)));
     }
+    else
+    {
+        Console.WriteLine("no last key");
+    }
+    lastKey = tiltedToString(tilted);
 }
-Console.WriteLine($"Cache hits: {cacheHits}, cache misses: {cacheMisses}, cache size: {cache.Count}");
 
 tilted.ToList().ForEach(a =>
             Console.WriteLine(new string(a)));
@@ -83,4 +108,15 @@ long sum = tilted.Select((a, i) => (a, i)).Sum(a => a.a.Count(b => b == 'O') * (
 Console.WriteLine(sum);
 
 Console.WriteLine((tilted.Length, tilted[0].Length));
+
+cache_next[lastKey].Item2.ToList().ForEach(a =>
+            Console.WriteLine(new string(a)));
+
+// foreach (var key in cache_next.Keys)
+// {
+//     Console.WriteLine($"Key:  {key}\nnext: {cache_next[key].Item1}");
+//     cache_next[key].Item2.ToList().ForEach(a =>
+//             Console.WriteLine(new string(a)));
+//     Console.WriteLine();
+// }
 
